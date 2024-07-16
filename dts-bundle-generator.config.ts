ef7bfced789434c16bd0de77a3398d4a3de42939
Matronator <5470780+matronator@users.noreply.github.com>
2024-07-16 12:07:54 +0200

    update pkg and vite

--- dts-bundle-generator.config.ts
+++ dts-bundle-generator.config.ts
@@ -2,7 +2,7 @@ const config = {
     entries: [
         {
             filePath: "./src/index.ts",
-            outFile: "./dist/package-name.d.ts",
+            outFile: "./dist/view-sync.d.ts",
             noCheck: false,
         },
     ],
